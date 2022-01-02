602f9fb01e321d92d15c78a3857786ff75595fea
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-02 16:55:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Write a python program to perform all arithmetic operations (Give Numbers Directly)
+#A. Add Two Numbers
+#B. Difference of Two Numbers
+#C. Product of Two Numbers
+#D. Quotient of Two Numbers
+#E. Remainder of Two Numbers
+#F. Exponent of a number
+x=int(input())
+y=int(input())
+print(x + y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
